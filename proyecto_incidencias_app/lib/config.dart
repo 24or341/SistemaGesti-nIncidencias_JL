705394b561d4da82fef0c2153a4c7cd478ed4e32
Leandro <lh2015052384@virtual.upt.pe>
2025-11-21 00:35:52 -0500

    Actualizar despliegue

--- proyecto_incidencias_app/lib/config.dart
+++ proyecto_incidencias_app/lib/config.dart
@@ -2,5 +2,5 @@
 //Conexion Local
 //const String baseUrl = 'http://10.0.2.2/SistemaGesti-nIncidencias_JL/gestion_incidencias_web/api/public/';
 
-//Conexion remota
-const String baseUrl = 'http://20.63.24.52/SistemaGesti-nIncidencias_JL/gestion_incidencias_web/api/public/';
+//Conexion Remota
+const String baseUrl = 'http://incidenciaadmin.sytes.net/api/public/';
